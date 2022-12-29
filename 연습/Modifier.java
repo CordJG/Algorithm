78a80437e6026027c9edbbc52146f28c3ae5e724
CordJG <wjrmffldglem@gmail.com>
2022-12-29 14:04:59 +0900

    commit

---
+++ 연습/Modifier.java
@@ -0,0 +1,30 @@
+package 연습;
+
+public class Modifier {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+
+        p.printEach();
+
+        //System.out.println(p.a);
+        System.out.println(p.b);
+        System.out.println(p.c);
+        System.out.println(p.d);
+    }
+}
+
+class Parent{
+    private int a = 2;
+    int b = 3;
+    protected int c = 4;
+    public double d = 5.0;
+
+    public void printEach(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+
+
+}
