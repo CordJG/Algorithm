cb6a2cff65ce7eacbd09bf1831687a64067d4fd0
CordJG <wjrmffldglem@gmail.com>
2022-12-30 15:08:48 +0900

    commit

---
+++ 연습/InterfaceExample.java
@@ -0,0 +1,20 @@
+package 연습;
+
+public class InterfaceExample {
+    public static void main(String[] args) {
+        User user = new User();
+        user.callProvider(new Provider());
+    }
+}
+
+class User {
+    public void callProvider(Provider provider){
+        provider.call();
+    }
+}
+
+class Provider {
+    public void call() {
+        System.out.println("무야호~");
+    }
+}
