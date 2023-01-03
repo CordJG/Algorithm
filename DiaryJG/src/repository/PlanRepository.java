a60404881e108fb4402eb6a38609c6310c4562ee
CordJG <wjrmffldglem@gmail.com>
2023-01-03 23:34:28 +0900

    commit

---
+++ DiaryJG/src/repository/PlanRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import diary.write.Plan;
+
+public class PlanRepository {
+
+    Plan[] plans= new Plan[0];
+
+}
---
+++ DiaryJG/src/repository/RetrospectRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import diary.write.Plan;
+import diary.write.Retrospect;
+
+public class RetrospectRepository {
+
+    Retrospect[] retrospects= new Retrospect[0];
+}
