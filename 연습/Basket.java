74c0d4d379aec4a6a86741b8af6ad510648edeb5
CordJG <wjrmffldglem@gmail.com>
2023-01-04 15:16:22 +0900

    commit

---
+++ 연습/Basket.java
@@ -0,0 +1,34 @@
+package 연습;
+
+public class Basket <T>{
+    private T item;
+
+    public Basket(T item){
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item){
+        this.item = item;
+    }
+}
+
+class Item{
+    String item;
+
+    public static void main(String[] args) {
+
+
+        Basket<Boolean> basket = new Basket<Boolean>(true);
+
+        System.out.println(basket.getItem());
+
+
+
+
+
+    }
+}
