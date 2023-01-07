019121d11afc5c5dffe12d0f71566861c292c154
CordJG <wjrmffldglem@gmail.com>
2023-01-08 00:49:36 +0900

    commit

--- 연습3/StreamArray2.java
+++ 연습3/StreamArray2.java
@@ -1,14 +1,15 @@
 package 연습3;
-import java.util.stream.Stream;
-import java.util.*;
 
-public class StreamArray {
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamArray2 {
     public static void main(String[] args) {
         //문자열 배열 선언 및 할당
         String[] arr = new String[]{"김코딩", "이자바", "박해커"};
 
         // 문자열 스트림 생성
-        Stream<String> stream = Arrays.stream(arr);
+        Stream<String> stream = Stream.of(arr);
 
         // 출력
         stream.forEach(System.out::println);
