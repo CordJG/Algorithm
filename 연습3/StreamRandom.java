ab8c07bfe76269ad9788aeb644c3a788d207e974
CordJG <wjrmffldglem@gmail.com>
2023-01-08 01:50:54 +0900

    commit

--- 연습3/StreamRandom.java
+++ 연습3/StreamRandom.java
@@ -7,7 +7,11 @@ public class StreamRandom {
     public static void main(String[] args) {
         // 난수 생성
         //IntStream ints = new Random().ints(5);
-        IntStream ints = new Random().ints().limit(5);
-        ints.forEach(System.out::println);
+        //IntStream ints = new Random().ints().limit(5);
+        //ints.forEach(System.out::println);
+
+        //특정 범위의 정수
+        IntStream intStream = IntStream.rangeClosed(1, 20);
+        intStream.forEach(System.out::println);
     }
 }
