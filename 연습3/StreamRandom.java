19f466eb6c88ed81637e8c89d3c6a228b598a901
CordJG <wjrmffldglem@gmail.com>
2023-01-08 01:45:54 +0900

    commit

---
+++ 연습3/StreamRandom.java
@@ -0,0 +1,12 @@
+package 연습3;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamRandom {
+    public static void main(String[] args) {
+        // 난수 생성
+        IntStream ints = new Random().ints();
+        ints.forEach(System.out::println);
+    }
+}
