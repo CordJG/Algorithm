eb826fa5d3b6ac87f613bcbf4affbd49d4c4c701
CordJG <wjrmffldglem@gmail.com>
2023-01-09 01:00:34 +0900

    commit

--- 연습3/FlatMap.java
+++ 연습3/FlatMap.java
@@ -3,10 +3,18 @@ import java.util.*;
 
 public class FlatMap {
     public static void main(String[] args) {
-        String[][] namesArray = new String[][]{{"박해커", "이자바"},{"김코딩", "나박사"}};
+        String[][] namesArray = new String[][]{{"박해커", "이자바"},{"김코딩", "나박사"},{"이재관"}};
 
         Arrays.stream(namesArray)
                 .map(inner -> Arrays.stream(inner))
                 .forEach(System.out::println);
+        System.out.println();
+
+        Arrays.stream(namesArray)
+                .map(inner -> Arrays.stream(inner))
+                .forEach(names -> names.forEach(System.out::println));
+        System.out.println();
+
+        Arrays.stream(namesArray).flatMap(Arrays::stream).forEach(System.out::println);
     }
 }
---
+++ 연습3/StreamSorted.java
@@ -0,0 +1,17 @@
+package 연습3;
+import java.util.*;
+
+public class StreamSorted {
+    public static void main(String[] args) {
+
+        List<String> animals = Arrays.asList("Tiger", "Lion", "Monkey", "Duck", "Horse", "Cow");
+        List<String> names = Arrays.asList("이재관", "조영현", "김민주", "이수지", "최진영");
+
+        animals.stream()
+                .sorted()
+                .forEach(System.out::println);
+        System.out.println();
+
+        names.stream().sorted().forEach(System.out::println);
+    }
+}
