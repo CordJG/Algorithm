2c3a794fc89c1a0208a78b54c0f7d7d4d00b56fe
CordJG <wjrmffldglem@gmail.com>
2023-01-09 00:28:55 +0900

    commit

---
+++ 연습3/StreamMap.java
@@ -0,0 +1,23 @@
+package 연습3;
+import java.util.*;
+
+public class StreamMap {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("kimcoding", "LeeJaeGwan", "GwAqEwTt", "javautile");
+        names.stream()
+                .map(element -> element.toUpperCase())
+                .forEach(element-> System.out.println(element));
+        System.out.println();
+
+        List<Integer> list = Arrays.asList(1, 3, 6, 9);
+
+        list.stream()
+                .map(number -> number * 3).forEach(System.out::println);
+        System.out.println();
+
+        list.stream()
+                .map(number -> number *4).forEach(number-> System.out.println(number));
+
+    }
+
+}
