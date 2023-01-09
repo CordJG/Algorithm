04ee6c6c62fc7c63f95a3a882eb0881bfd40f01d
CordJG <wjrmffldglem@gmail.com>
2023-01-09 15:53:57 +0900

    commit

--- 연습4/FileInputStreamExample.java
+++ 연습4/FileInputStreamExample.java
@@ -1,11 +1,13 @@
 package 연습4;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 
 public class FileInputStreamExample {
     public static void main(String[] args) {
         try{
             FileInputStream fileInput = new FileInputStream("codestates.txt");
+            BufferedInputStream bufferedInput = new BufferedInputStream(fileInput);
             int i = 0;
             while ((i = fileInput.read()) != -1) {
                 System.out.println((char)i);
