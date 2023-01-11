e1979cc135e917b29add9dccc0a34fd0b6077238
CordJG <wjrmffldglem@gmail.com>
2023-01-11 23:21:05 +0900

    commit

--- 연습5/CordJgex.java
+++ 연습5/CordJgex.java
@@ -9,7 +9,27 @@ public class CordJgex {
         }
     }
 
+    public static int f2(int n) {
+        int num1 = 1;
+        int num2 = 1;
+        int sumNum = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i <= 2) sumNum = 1;
+            else {
+                num1 = num2;
+                num2 = sumNum;
+                sumNum = num1 + num2;
+
+            }
+
+        }
+        return sumNum;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(f(40));
+        System.out.println(f(30));
+        System.out.println(f2(30));
     }
 }
