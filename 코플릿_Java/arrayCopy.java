7811990390917a242bcd7ed6c6b4e2a4a5ceb8ec
CordJG <wjrmffldglem@gmail.com>
2023-01-13 00:11:28 +0900

    commit

--- 코플릿_Java/arrayCopy.java
+++ 코플릿_Java/arrayCopy.java
@@ -7,16 +7,15 @@ public class arrayCopy {
     static int[] b = {6,7,8,9,10};
 
     static int[] c = new int[a.length+b.length];
+    static int[] d = new int[a.length+b.length];
 
     public static void main(String[] args) {
         System.arraycopy(a,0,c,0,a.length);
         System.arraycopy(b,0,c,a.length,b.length);
-
-
-        for(int i=0; i<c.length; i++){
-            System.out.println(c[i]);
-        }
+        int[] d = Arrays.copyOf(a,5);
+        d = Arrays.copyOf(b, 4);
 
         System.out.println(Arrays.toString(c));
+        System.out.println(Arrays.toString(d));
     }
 }
