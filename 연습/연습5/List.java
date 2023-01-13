419b2c2ed46c1fce42dbdfbeac812b5d1b84c192
CordJG <wjrmffldglem@gmail.com>
2023-01-13 23:53:59 +0900

    commit

--- 연습/연습5/List.java
+++ 연습/연습5/List.java
@@ -1,4 +1,27 @@
 package 연습.연습5;
 
+import java.util.ArrayList;
+import java.util.random.*;
+
 public class List {
+    public static void main(String[] args) {
+        boolean babo=false;
+        ArrayList<Boolean> list = new ArrayList<>(50);
+        list.add(true);
+        list.add(babo);
+        list.add(false);
+
+        for(int i=1; i<=47 ; i++){
+            int num= (int)(Math.random()*2+1);
+            if(num==1) list.add(true);
+            else list.add(false);
+        }
+
+        for(boolean bool : list) {
+
+            System.out.println(String.valueOf(bool));
+        }
+
+
+    }
 }
