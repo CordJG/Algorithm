2dcbf0fb4a06ab40ce5c6b6863893f21a9f92cd8
CordJG <wjrmffldglem@gmail.com>
2023-01-14 01:01:02 +0900

    commit

--- 연습/연습5/List.java
+++ 연습/연습5/List.java
@@ -6,22 +6,19 @@ import java.util.random.*;
 public class List {
     public static void main(String[] args) {
         boolean babo=false;
-        ArrayList<Boolean> list = new ArrayList<>(50);
+        ArrayList<Boolean> list = new ArrayList<>(20);
         list.add(true);
         list.add(babo);
         list.add(false);
 
-        for(int i=1; i<=47 ; i++){
+        for(int i=1; i<=17 ; i++){
             int num= (int)(Math.random()*2+1);
             if(num==1) list.add(true);
             else list.add(false);
         }
-
         for(boolean bool : list) {
 
             System.out.println(String.valueOf(bool));
         }
-
-
     }
 }
