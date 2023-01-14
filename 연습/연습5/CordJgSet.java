62837b3f92af94e1ca809f4d6c9121bb10c46c6d
CordJG <wjrmffldglem@gmail.com>
2023-01-14 17:46:32 +0900

    commit

---
+++ 연습/연습5/CordJgSet.java
@@ -0,0 +1,18 @@
+package 연습.연습5;
+import java.util.*;
+
+public class CordJgSet {
+    public static void main(String[] args) {
+
+        HashSet<Integer> hashSet = new HashSet<>(10);
+
+        hashSet.add(10);
+
+        System.out.println(hashSet.add(20));
+        Iterator<Integer> iter = hashSet.iterator();
+        while(iter.hasNext()){
+            int num = iter.next();
+            System.out.println(num);
+        }
+    }
+}
