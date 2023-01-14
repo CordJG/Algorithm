3788675a44dbbda5d53cef5630248ace616945df
CordJG <wjrmffldglem@gmail.com>
2023-01-15 00:08:28 +0900

    commit

---
+++ 연습/연습5/CordJgSubString.java
@@ -0,0 +1,15 @@
+package 연습.연습5;
+
+public class CordJgSubString {
+    public static void main(String[] args) {
+        String str = "ABCDEFG";
+
+        String result = str.substring(3);
+        System.out.println(result);
+
+        result = str.substring(3,6);
+        System.out.println(result);
+
+
+    }
+}
