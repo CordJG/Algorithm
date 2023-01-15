35bb3c8c3359bdd847b9d9c357a0c630a7766010
CordJG <wjrmffldglem@gmail.com>
2023-01-15 22:47:40 +0900

    commit

--- 연습/연습6/CordJgReverse.java
+++ 연습/연습6/CordJgReverse.java
@@ -3,6 +3,7 @@ package 연습.연습6;
 public class CordJgReverse {
     public static void main(String[] args) {
         String str = "I love java really";
+        int num = 2;
 
         StringBuilder strBuild = new StringBuilder(str);
         String reverseStr = strBuild.reverse().toString();
