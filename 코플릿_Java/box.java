c06e2e6a049782661f0e308ac0156327cbbddaf7
CordJG <wjrmffldglem@gmail.com>
2023-01-16 21:49:17 +0900

    commit

--- 코플릿_Java/box.java
+++ 코플릿_Java/box.java
@@ -8,23 +8,24 @@ public class box {
         Integer[] boxes = new Integer[]{10,1,3,5,2,7,11,2,3,4,5,6,7,8,9,10};
 
         List<Integer> list = Arrays.asList(boxes);
-        int num=0;
-        int count =1;
-        int count2 =1;
+        int length=0;
 
-        for(int i=1; i<boxes.length; i++){
-            if(boxes[0]>boxes[i]){
-              count++;
-              num=list.indexOf(i);
-            }
-        }
-        for(int j=num; j<boxes.length; j++){
-            if(boxes[num]>boxes[j+1]){
-                count2++;
-            }
+        List<Integer> lengthes = new ArrayList<>();
+
+        for(int i=0; i<boxes.length; i++){
+            int j=i+1;
+            for(; j<boxes.length; j++)
+                if(list.get(i)<list.get(j))
+                    break;
+
+                length=j-i;
+                lengthes.add(length);
+                i=j-1;
         }
 
-        int n = Math.max(count, count2);
+        System.out.println(lengthes);
+        int max = Collections.max(lengthes);
+        System.out.println(max);
 
 
 
