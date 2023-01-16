7e137796d93d5a4fb3004e280a4645d9bb8622f8
CordJG <wjrmffldglem@gmail.com>
2023-01-16 11:06:28 +0900

    commit

--- 코플릿_Java/DailyCoding/letterCapitalize.java
+++ 코플릿_Java/DailyCoding/letterCapitalize.java
@@ -4,21 +4,30 @@ import java.util.*;
 
 public class letterCapitalize {
     public static void main(String[] args) {
-        String str = "nVERR  gIVE uP";
-        String[] str2 = str.split(" ");
-        List<String> str3 = Arrays.asList(str2);
-        Iterator<String> it = str3.iterator();
+        String str = "i love java  really ";
         String result ="";
-        String str4First ="";
-        String str4Remain="";
-        while(it.hasNext()){
-            String str4 = it.next();
-            if(str4 != " ")
-                str4First = str4.substring(0,1);
-            str4First = str4First.toUpperCase();
-            str4Remain = str4.substring(1);
-            str4 = str4First+str4Remain;
-            result += str4+" ";
+
+        String[] str2 = str.split(" ");
+        if(str2.length==0) str=result;
+        else {
+            System.out.println(Arrays.toString(str2));
+            List<String> str3 = Arrays.asList(str2);
+            Iterator<String> it = str3.iterator();
+
+            String str4First = "";
+            String str4Remain = "";
+            while (it.hasNext()) {
+                String str4 = it.next();
+
+                if (str4.length() != 0) {
+                    str4First = str4.substring(0, 1);
+                    str4First = str4First.toUpperCase();
+                    str4Remain = str4.substring(1);
+                    str4 = str4First + str4Remain;
+                }
+                result += str4 + " ";
+
+            }
         }
         result = result.substring(0, str.length());
 
