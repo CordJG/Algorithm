f2ab5cb4eec9d994d635866e2518f487a6e85ec9
CordJG <wjrmffldglem@gmail.com>
2023-01-16 09:48:50 +0900

    commit

--- 코플릿_Java/DailyCoding/letterCapitalize.java
+++ 코플릿_Java/DailyCoding/letterCapitalize.java
@@ -4,27 +4,24 @@ import java.util.*;
 
 public class letterCapitalize {
     public static void main(String[] args) {
-
-        String str = "i love  java";
-        String a=String.valueOf(str.charAt(2)).toUpperCase();
-        System.out.println(a);
-
-
+        String str = "nVERR  gIVE uP";
         String[] str2 = str.split(" ");
-
         List<String> str3 = Arrays.asList(str2);
         Iterator<String> it = str3.iterator();
         String result ="";
-        String word="";
-        while(it.hasNext()) {
+        String str4First ="";
+        String str4Remain="";
+        while(it.hasNext()){
             String str4 = it.next();
-            if (str4 != " ")
-                for (int i = 1; i < str4.length(); i++) {
-                    word += String.valueOf(str4.charAt(0)).toUpperCase() + str4.charAt(i);
-                }
-                    result += word + " ";
-
+            if(str4 != " ")
+                str4First = str4.substring(0,1);
+            str4First = str4First.toUpperCase();
+            str4Remain = str4.substring(1);
+            str4 = str4First+str4Remain;
+            result += str4+" ";
         }
+        result = result.substring(0, str.length());
+
         System.out.println(result);
 
 
