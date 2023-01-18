6a5dbcf3076c897549bc779c8042c0450a13a001
CordJG <wjrmffldglem@gmail.com>
2023-01-19 00:36:57 +0900

    commit

---
+++ 연습/연습6/anything.java
@@ -0,0 +1,22 @@
+package 연습.연습6;
+import java.util.*;
+
+public class anything {
+    public static void main(String[] args) {
+        String[][] arr = {};
+            HashMap<String, String> map = new HashMap<>();
+
+
+
+
+            for(int i=0; i< arr.length; i++){
+                map.put(arr[i][0], arr[i][1]);
+                }
+
+        if(arr.length==0) {map=map;}
+        System.out.println(map);
+
+
+            System.out.println(map);
+        }
+    }
