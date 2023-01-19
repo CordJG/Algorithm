618d91eee0184e86098c9bd90576da30ee4e117b
CordJG <wjrmffldglem@gmail.com>
2023-01-19 18:35:13 +0900

    commit

--- 연습/연습6/anything.java
+++ 연습/연습6/anything.java
@@ -3,20 +3,20 @@ import java.util.*;
 
 public class anything {
     public static void main(String[] args) {
-        String[][] arr = {};
-            HashMap<String, String> map = new HashMap<>();
+        Integer[] arr = {1,2,3,4,5,6,7,8,9,10};
+        List<Integer> list = Arrays.asList(arr);
 
 
-
-
-            for(int i=0; i< arr.length; i++){
-                map.put(arr[i][0], arr[i][1]);
+        for(int i=0; i<5; i++){
+            for(int j=9; j>1; j--){
+                if(list.get(i)+list.get(j)<=6){
+                    System.out.println(i);
+                    System.out.println(j);
+                    System.out.println(list.get(i));
+                    System.out.println(list.get(j));
                 }
+            }
+        }
 
-        if(arr.length==0) {map=map;}
-        System.out.println(map);
-
-
-            System.out.println(map);
         }
     }
