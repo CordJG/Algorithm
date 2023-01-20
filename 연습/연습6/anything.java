dc4db532768c94945c8db0c9f27fcc20c6636040
CordJG <wjrmffldglem@gmail.com>
2023-01-20 12:46:00 +0900

    commit

--- 연습/연습6/anything.java
+++ 연습/연습6/anything.java
@@ -3,20 +3,13 @@ import java.util.*;
 
 public class anything {
     public static void main(String[] args) {
-        Integer[] arr = {1,2,3,4,5,6,7,8,9,10};
-        List<Integer> list = Arrays.asList(arr);
+        Integer[] arr = {1,2,3,4,5};
+        List<Integer> list = new ArrayList<>(Arrays.asList(arr));
 
+        list.add(1);
 
-        for(int i=0; i<5; i++){
-            for(int j=9; j>1; j--){
-                if(list.get(i)+list.get(j)<=6){
-                    System.out.println(i);
-                    System.out.println(j);
-                    System.out.println(list.get(i));
-                    System.out.println(list.get(j));
-                }
-            }
-        }
+        System.out.println(list);
 
-        }
     }
+}
+
