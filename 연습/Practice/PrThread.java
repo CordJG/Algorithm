39ec77980bc91ead6cd3f58d73a28d53e3d66b8d
CordJG <wjrmffldglem@gmail.com>
2023-01-21 17:36:06 +0900

    commit

--- 연습/Practice/PrThread.java
+++ 연습/Practice/PrThread.java
@@ -1,29 +1,30 @@
 package 연습.Practice;
 
-public class PrThread {
-    public static void main(String[] args) {
-        A a = new A();
-        B b = new B();
-        a.play();
-        System.out.println();
-        b.play();
+public class PrThread extends Thread {
 
-    }
-}
+    String name ;
 
-class A {
-    void play() {
+    public PrThread(String a){name=a;}
+
+    public void run() {
+
+        System.out.println("내 이름은 "+name+"입니다");
         for (int i = 0; i < 4; i++) {
             System.out.println("첫번째 " + i + " 입니다");
         }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"이재관","고양이","코딩"};
+        for(int i=0; i<a.length; i++) {
+            PrThread pr = new PrThread(a[i]);
+            pr.start();
+            System.out.println();
+        }
     }
 }
 
-    class B {
-        void play() {
-            for (int i = 0; i < 4; i++) {
-                System.out.println("두번째 " + i + " 입니다");
-            }
-        }
-    }
+
+
 
--- 연습/연습7/CordThread2.java
+++ 연습/연습7/CordThread2.java
@@ -7,15 +7,7 @@ public class CordThread2 extends Thread {
         skill_name = cmd;
     }
 
-    public void run() { // start() 메소드 사용 시
-        System.out.println("시전한 스킬 : " + skill_name);
 
-        for(int i = 1; i<=3; i++) {
-            System.out.println(skill_name + " 스킬을 " + i + "초간 시전 중입니다..");
-        }
-
-        System.out.println("시전 끝난 스킬 : " + skill_name);
-    }
 
     public static void main(String[] args) {
         String[] cmd = new String[] {"Q", "W", "E"};
@@ -24,6 +16,20 @@ public class CordThread2 extends Thread {
             CordThread2 a = new CordThread2(cmd[i]);
             a.start(); // extends Thread 후 start() 메소드 사용 시 run() 메소드 수행하게 됨
         }
+    }
+}
+
+class Aari{
+
+    CordThread2 cord = new CordThread2("매혹");
+    public void run() { // start() 메소드 사용 시
+        System.out.println("시전한 스킬 : " + cord.skill_name);
+
+        for(int i = 1; i<=3; i++) {
+            System.out.println(cord.skill_name + " 스킬을 " + i + "초간 시전 중입니다..");
+        }
+
+        System.out.println("시전 끝난 스킬 : " + cord.skill_name);
     }
 }
 
