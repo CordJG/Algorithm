4d5d9e64bdf5b2423dbf1bc40e154e44880c25c6
CordJG <wjrmffldglem@gmail.com>
2023-01-29 14:51:46 +0900

    commit

--- 연습/Practice/Wrapper.java
+++ 연습/Practice/Wrapper.java
@@ -1,25 +1,10 @@
 package 연습.Practice;
+import java.util.*;
 
 public class Wrapper {
     public static void main(String[] args) {
 
-        boolean a ;
-        Integer num1 = new Integer(43);
-        Integer num2 = new Integer("43");
-
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(num1+num2);
-        int iNum1 = 43;
-        int iNum2 = 43;
-        //System.out.println(iNum1.compareTo(iNum2));   //-> 오류뜸
-        System.out.println(num1.compareTo(num2));
-        a= (num2==num1);
-        System.out.println(a);
-
-        Integer n1 = 4561;
-        Integer n2 = 4561;
-        if(n1==n2)
-        System.out.println("헤헤");
+       List<int> list = new ArrayList<>();
+       List<Integer> list2 = new ArrayList<>();
     }
 }
