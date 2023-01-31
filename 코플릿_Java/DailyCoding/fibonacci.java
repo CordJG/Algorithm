67bc2ff3b46b9922d1dc2e91bf2f201e54f68c09
CordJG <wjrmffldglem@gmail.com>
2023-01-31 10:21:24 +0900

    commit

--- 코플릿_Java/DailyCoding/fibonacci.java
+++ 코플릿_Java/DailyCoding/fibonacci.java
@@ -4,27 +4,28 @@ import java.util.*;
 
 public class fibonacci {
     public static void main(String[] args) {
+        Fibo fibo = new Fibo();
 
-        // f(n) = f(n-1)+f(n-2);
-        // f(1) = 1
-        // f(0) = 0
-
-
-
+        int fiboNum= fibo.fibona(3);
+        System.out.println(fiboNum);
     }
 }
 
-class any{
-    List<Integer> list = new ArrayList<>();
-
+class Fibo{
 
     int fibona(int num){
-        list.add(0);
-        list.add(1);
-        if(num==0) return 0;
-        if(num==1) return 1;
 
-        return fibona(num-1)+fibona(num-2);
+        int[] arr = new int[1000];
 
+        if(arr[num]!=0){
+        }else{
+            if(num==0) arr[num]=0;
+            else if(num==1) arr[num]=1;
+            else{
+                arr[num] = fibona(num-1)+fibona(num-2);
+            }
+        }
+        return arr[num];
     }
 }
+
