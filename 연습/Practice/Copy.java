777c907ea61f626dbe8ae971c0d7540b4fb08028
CordJG <wjrmffldglem@gmail.com>
2023-02-01 19:15:21 +0900

    commit

--- 연습/Practice/Copy.java
+++ 연습/Practice/Copy.java
@@ -4,14 +4,31 @@ import java.util.Arrays;
 
 public class Copy {
     public static void main(String[] args) {
-        int[] CordJg = {1,2,3,4,5};
-        int[] Cat = CordJg.clone();
+        Integer[][] CordJg = {{1,2},{4,5}};
+        Integer[][] Cat = new Integer[CordJg.length][CordJg[0].length];
 
-        System.out.println(Arrays.toString(Cat));
+        for(int i=0; i< CordJg.length; i++){
+            for(int j=0; j<CordJg[i].length; j++){
+                Cat[i][j] = CordJg[i][j];
+            }
+        }
 
-        Cat[4]= 100;
+        System.out.println(Arrays.deepToString(Cat));
 
-        System.out.println(Arrays.toString(Cat));
-        System.out.println(Arrays.toString(CordJg));
+        Cat[0][0]= 100;
+
+        System.out.println(Arrays.deepToString(Cat));
+        System.out.println(Arrays.deepToString(CordJg));
+
+        Integer[][] Dog = new Integer[CordJg.length][CordJg[0].length];
+
+        for(int i=0; i< CordJg.length; i++){
+            System.arraycopy(CordJg[i],0,Dog[i],0,Dog[i].length);
+        }
+
+        Dog[0][0]= 200;
+
+        System.out.println(Arrays.deepToString(Dog));
+        System.out.println(Arrays.deepToString(CordJg));
     }
 }
