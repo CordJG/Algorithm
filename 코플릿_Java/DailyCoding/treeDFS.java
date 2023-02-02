046e1b38c24deb56a7fdd769f7ecbca440ee6112
CordJG <wjrmffldglem@gmail.com>
2023-02-02 10:35:54 +0900

    commit

---
+++ 코플릿_Java/DailyCoding/treeDFS.java
@@ -0,0 +1,34 @@
+package 코플릿_Java.DailyCoding;
+import java.util.*;
+public class treeDFS {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static class tree {
+        private String value;
+        private ArrayList<tree> children;
+
+        public tree(String data) {
+            this.value = data;
+            this.children = null;
+        }
+
+        public tree addChildNode(tree node) {
+            if (children == null) children = new ArrayList<>();
+            children.add(node);
+            return children.get(children.size() - 1);
+        }
+
+        public String getValue() {      //현재 노드의 데이터를 반환
+            return value;
+        }
+
+        public ArrayList<tree> getChildrenNode() {
+            return children;
+        }
+    }
+}
+
