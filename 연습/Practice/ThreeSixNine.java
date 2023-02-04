02085551425edbe42a0b6988e9ab636d11227b39
CordJG <wjrmffldglem@gmail.com>
2023-02-04 19:48:05 +0900

    commit

---
+++ 연습/Practice/ThreeSixNine.java
@@ -0,0 +1,52 @@
+package 연습.Practice;
+
+import java.util.*;
+
+public class ThreeSixNine {
+    public static void main(String[] args) {
+
+      Play play = new Play();
+
+        System.out.println(play.call2(369));
+
+
+    }
+}
+
+class Play{
+
+    List<String> list = new ArrayList<>();
+    String call(int num){
+        String str = String.valueOf(num);
+        char[] chars = new char[str.length()];
+
+        for(int i=0; i<str.length(); i++){
+            chars[i]= str.charAt(i);
+
+        }
+
+        char[] nums = new char[]{'3','6','9'};
+        int count=0;
+
+        for(int j=0; j<chars.length; j++){
+            for(int k=0; k<nums.length; k++){
+                if(chars[j] == nums[k]){
+                    count++;
+                    break;
+                }
+            }
+        }
+        String result= "짝";
+        if(count==0) return "x";
+
+        return result.repeat(count);
+
+    }
+
+    List<String> call2(int num){
+        for(int i=1; i<=num; i++){
+            list.add(call(i));
+        }
+        return list;
+    }
+}
