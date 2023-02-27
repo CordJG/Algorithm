5089471808c52450cd12f186ed49cd577c83b94e
CordJG <wjrmffldglem@gmail.com>
2023-02-27 13:33:48 +0900

    commit

--- 정리용/codingTest/silver/TimeManagement1263.java
+++ 정리용/codingTest/silver/TimeManagement1263.java
@@ -5,37 +5,22 @@ import java.util.*;
 public class TimeManagement1263 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int toDoNum = sc.nextInt();
-
-        //음...2차원배열로 하는 게 제일 편할듯
-
-        int[][] timeTable = new int[toDoNum][2];
-        for (int i = 0; i < toDoNum; i++) {
-            timeTable[i][0] = sc.nextInt();
-            timeTable[i][1] = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] times = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            times[i][0] = sc.nextInt(); // 업무를 끝내야 하는 시간
+            times[i][1] = sc.nextInt(); // 업무를 수행하는데 필요한 시간
         }
-
-        Arrays.sort(timeTable, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[1], o2[1]);
+        Arrays.sort(times, new Comparator<int[]>() { // 업무를 끝내야 하는 시간 기준으로 정렬
+            public int compare(int[] a, int[] b) {
+                return b[0] - a[0];
             }
         });
-
-        System.out.println(Arrays.deepToString(timeTable));
-
-        int sleepTime = 0;
-        for (int i = 1; i < toDoNum; i++) {
-
-            int startTime = timeTable[i][1] - timeTable[i][0];
-            if (startTime >= sleepTime) {
-                sleepTime = startTime;
-            } else {
-                sleepTime -= timeTable[i][0];
-                sleepTime = Math.max(sleepTime, startTime);
-            }
+        int end = times[0][0] - times[0][1]; // 첫 번째 업무의 시작 시간
+        for (int i = 1; i < n; i++) {
+            int curEnd = times[i][0] - times[i][1]; // 현재 업무를 시작할 수 있는 가장 늦은 시간
+            end = Math.min(end, curEnd); // 시작 시간 갱신
         }
-        System.out.println(sleepTime);
+        System.out.println(end >= 0 ? end : -1); // 결과 출력
     }
 }
