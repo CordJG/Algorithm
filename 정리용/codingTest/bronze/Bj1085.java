4376d43fde83e5c7dee9466a43669926751039c3
CordJG <wjrmffldglem@gmail.com>
2023-06-14 04:20:55 +0900

    백준 1085 , 1247 브론즈3 (1085 낚시 ㄷㄷ;)

---
+++ 정리용/codingTest/bronze/Bj1085.java
@@ -0,0 +1,35 @@
+package codingTest.bronze;
+
+import java.util.Scanner;
+
+public class Bj1085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nums = new int[4];
+        String[] line = sc.nextLine().split(" ");
+        for (int i = 0; i < 4; i++) {
+            nums[i] = Integer.parseInt(line[i]);
+        }
+        int minimumN = findNum(nums);
+        System.out.println(minimumN);
+
+    }
+
+    static int findNum(int[] nums) {
+        int x = nums[0];
+        int y = nums[1];
+        int w = nums[2];
+        int h = nums[3];
+
+        int minimumN = 1001;
+
+        int[] numbers = {x, y, Math.abs(w - x), Math.abs(h - y)};
+        for (int num : numbers) {
+            if (minimumN > num) {
+                minimumN = num;
+            }
+        }
+        return minimumN;
+    }
+}
---
+++ 정리용/codingTest/bronze/Bj1247.java
@@ -0,0 +1,44 @@
+package codingTest.bronze;
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Bj1247 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger[][] nums = new BigInteger[3][];
+
+        for (int i = 0; i < 3; i++) {
+            int N = sc.nextInt();
+            nums[i] = new BigInteger[N];
+
+            for (int j = 0; j < N; j++) {
+                nums[i][j] = sc.nextBigInteger();
+            }
+        }
+        sc.close();
+        for (BigInteger[] num : nums) {
+            System.out.println(findSymbol(num));
+        }
+
+    }
+
+    static String findSymbol(BigInteger[] nums) {
+        BigInteger n = BigInteger.valueOf(0);
+        BigInteger zero = BigInteger.valueOf(0);
+        for (BigInteger num : nums) {
+            n = n.add(num);
+        }
+        if (n.compareTo(zero)==0) {
+            return "0";
+        }
+        else if (n.compareTo(zero)<0) {
+            return "-";
+        }
+        else if (n.compareTo(zero)>0) {
+            return "+";
+        }
+        throw new IllegalArgumentException("으앙");
+    }
+}
