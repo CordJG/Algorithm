3629d1e292a164834b632e3bbefe1ef47c64e47d
CordJG <wjrmffldglem@gmail.com>
2023-06-21 08:07:27 +0900

    백준 11723 집합 , 시간복잡도문제(집합을 다른 방식으로 구현)

--- 정리용/codingTest/silver/시간초과관련/Bj11723.java
+++ 정리용/codingTest/silver/시간초과관련/Bj11723.java
@@ -8,15 +8,17 @@ import java.io.*;
  *  switch 문 사용해서 간단하게 표현할 수 있다
  *  반복문마다 System.out 하는 것 보다
  *  StringBuilder 를 활용해서 하는 게 시간 훨씬 적게 든다! sb.append(set[n] ? 1 : 0).append('\n');
+ *  BufferWriter를 활용할 수도 있다 (stringBuilder대신 ) 이게 맞겠따.
  */
 
 public class Bj11723 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
 
         boolean[] set = new boolean[21];
-        StringBuilder sb = new StringBuilder();
+
 
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
@@ -31,7 +33,7 @@ public class Bj11723 {
                         set[n] = false;
                         break;
                     case "check":
-                        sb.append(set[n] ? 1 : 0).append('\n');
+                        bw.write(set[n] ? "1\n" : "0\n");
                         break;
                     case "toggle":
                         set[n] = !set[n];
@@ -52,6 +54,8 @@ public class Bj11723 {
                 }
             }
         }
-        System.out.println(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
