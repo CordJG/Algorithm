bc75c5849f51a1c03236b37d971c52cfea175848
CordJG <wjrmffldglem@gmail.com>
2023-06-29 23:56:31 +0900

    백준 12865

--- 정리용/codingTest/gold/dp/Bj12865.java
+++ 정리용/codingTest/gold/dp/Bj12865.java
@@ -4,31 +4,39 @@ import java.io.*;
 
 
 public class Bj12865 {
+
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int K = Integer.parseInt(input[1]);
+
+        int[] weights = new int[N + 1];
+        int[] values = new int[N + 1];
+        dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            input = br.readLine().split(" ");
+            weights[i] = Integer.parseInt(input[0]);
+            values[i] = Integer.parseInt(input[1]);
+        }
+
+        int result = knapsack(N, K, weights, values);
+        System.out.println(result);
     }
 
-    static class Bag {
-        private int weight;
-        private int value;
-
-        public Bag(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
+    public static int knapsack(int N, int K, int[] weights, int[] values) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if (weights[i] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], values[i] + dp[i - 1][j - weights[i]]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
-
-        public int getWeight(){
-            return weight;
-        }
-
-        public int getValue() {
-            return value;
-        }
+        return dp[N][K];
     }
-
 }
-
 
