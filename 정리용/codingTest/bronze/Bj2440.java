8e3a5b3c6fef1b699c8f207a2e69f6bc29f88fea
CordJG <wjrmffldglem@gmail.com>
2023-07-09 19:44:14 +0900

    백준 2440 별찍기 문제

--- 정리용/codingTest/bronze/Bj2440.java
+++ 정리용/codingTest/bronze/Bj2440.java
@@ -1,6 +1,8 @@
 package codingTest.bronze;
 
+import java.sql.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Bj2440 {
     public static void main(String[] args) {
@@ -12,5 +14,14 @@ public class Bj2440 {
             System.out.println(str.repeat(N));
             N = N - 1;
         }
+
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+
+        List<Integer> list = new ArrayList<>();
+
+        List<Integer> sortedList = (List<Integer>) list.stream().sorted();
+
     }
 }
