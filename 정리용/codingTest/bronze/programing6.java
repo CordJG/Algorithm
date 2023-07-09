9ec609effc656f2ec52c2eeb767c34d6b653778c
CordJG <wjrmffldglem@gmail.com>
2023-07-09 19:43:46 +0900

    백준 2440 별찍기 문제

---
+++ 정리용/codingTest/bronze/programing6.java
@@ -0,0 +1,33 @@
+package codingTest.bronze;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class programing6 {
+
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[] prices, int k) {
+
+        int minN = 100000;
+        for(int i=0; i<prices.length-k; i++){
+            minN=Math.min(prices[i],minN);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<k; i++){
+            list.add(prices[prices.length - 1 - i]);
+        }
+
+        int sumN=0;
+
+        List<Integer> sortedList = list.stream().sorted().collect(Collectors.toList());
+        for (int i = 0; i < k; i++) {
+            sumN += minN - sortedList.get(i);
+        }
+
+        return sumN;
+    }
+}
