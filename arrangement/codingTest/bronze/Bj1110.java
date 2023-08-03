219987ee0e918e75bd1db62b6e08ae7a7d97fd6e
CordJG <wjrmffldglem@gmail.com>
2023-08-03 14:58:10 +0900

    백준 1110

---
+++ arrangement/codingTest/bronze/Bj1110.java
@@ -0,0 +1,44 @@
+package codingTest.bronze;
+
+import java.util.Scanner;
+
+public class Bj1110 {
+    static int originN;
+    static int maxCount;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N < 10) {
+            N = N * 10;
+        }
+
+        originN = N;
+
+        int count =0;
+
+        int result = getCycleN(N, count);
+
+        System.out.println(result);
+
+    }
+
+    static int getCycleN(int N,int count) {
+        int num1 = N / 10;
+        int num2 = N - num1 * 10;
+
+        if (N == originN && count!=0) {
+            maxCount = count;
+            return maxCount;
+        }
+        int rightN = num1 + num2;
+        if (num1 + num2 >= 10) {
+            rightN = rightN-10;
+        }
+        int newN = num2 * 10 + rightN;
+        count++;
+
+
+
+        return getCycleN(newN, count);
+    }
+}
